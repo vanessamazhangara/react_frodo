74ea4c5b68f42d52a521f3bec5ca54f8b4dcba33
tienphandev <tienphan.dev@gmail.com>
2021-12-30 15:05:18 -0500

    Formatted Login.js and updated README with week 2 tasks

--- README.md
+++ README.md
@@ -30,7 +30,20 @@ Clone down the repo to your local machine. After git cloning the repo to your lo
  2. Styling is done inline and should be brief and to the point. (Hint: React inline styling is `style: {{ marginBottom: '10px'}}` so look up documentation if your confused!)
 <br/><br/>
 
-***Week 2 placeholder***
+## Week 2
+ 1. Create a new file in the `components` folder called `Input.js` 
+ 2. Inside `Input.js`, create a class component called `Input` and have it render all the necesary components to replicate the Frodo search field in the top right of the Frodo homepage <br/>(Hint: Use Chrome DevTools to inspect the page and see the HTML element tree)
+ 3. Initialize state inside the `Input` component with the attribute `query` set to an empty string. 
+ 4. Write a `handleChange` method inside the `Input` component that tracks the state of the query variable as the user is interacting with the input field. For example, if the user is searching for an assignment then we can monitor the state of this input field and capture the user's keystrokes.
+ 5. Create a new file in the `components` folder called `Footer.js`. This class component will render the entire footer recreated from Frodo. (Hint: Think of your component tree first inside the render() return of this `Footer.js` component. There will be several `divs` that hold several buttons. Use the Google Chrome extension Pesticide as a visual aide)   
+ 6. Create a new file in the `components` folder called `Header.js`. This class component will render the entire header and navigation bar recreated from Frodo. This includes the devmountain logo, the `Input` component from step 2, a dropdown menu, a nav bar with the tabs labeled `[lectures, exercises, further study, more (this should be a dropdown menu)]`
+
+
+ Keep in mind:
+ 1. You do not have to import the Footer or Header components yet. You can develop them in isolation and once they look like the real website versions, we will add them in week 3.
+ 2. Don't worry about routing to how to get to and/from this page or the login component. The buttons shouldn't work either.
+ 3. Consider our data model. Our `User` model at this point can be expected to have some sort of `userName`, `password`. As we build out the backend and API, we will introduce relationships such as students courses, assignments, grades and project pages.  
+<br/><br/>
 
 ***Week 3 placeholder***
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Login extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       userName: "",
-      password: ""
-    }
+      password: "",
+    };
   }
-    
-  
+
   render() {
     return (
-      <div style={{
-        width: '35%',
-        marginTop: '180px',
-        marginLeft: '6%',
-        textAlign: 'left',
-        boxSizing: 'border-box'
-        }}>
-          <h1 style={{
-            fontWeight: '500', 
-            lineHeight: '1.1',
+      <div
+        style={{
+          width: "35%",
+          marginTop: "180px",
+          marginLeft: "6%",
+          textAlign: "left",
+          boxSizing: "border-box",
+        }}
+      >
+        <h1
+          style={{
+            fontWeight: "500",
+            lineHeight: "1.1",
             fontFamily: "'Montserrat', sans-serif",
             margin: "0.5em 0",
-            fontSize: "20px"}}
-            >Welcome</h1>
-          <p >Your Devmountain adventure awaits.</p>
-          <form>
-            <div style={{marginBottom: "15px"}}>
-              <label htmlFor="login" style={{display: "block", fontWeight: "700", marginBottom:'2px'}}>Login</label>
-              <input type="text" id="login" onChange={(e) => this.setState({userName: e.target.value})} style={{
+            fontSize: "20px",
+          }}
+        >
+          Welcome
+        </h1>
+        <p>Your Devmountain adventure awaits.</p>
+        <form>
+          <div style={{ marginBottom: "15px" }}>
+            <label
+              htmlFor="login"
+              style={{
+                display: "block",
+                fontWeight: "700",
+                marginBottom: "2px",
+              }}
+            >
+              Login
+            </label>
+            <input
+              type="text"
+              id="login"
+              onChange={(e) => this.setState({ userName: e.target.value })}
+              style={{
                 display: "block",
                 width: "100%",
                 height: "34px",
@@ -44,12 +61,26 @@ export default class Login extends Component {
                 border: "1px solid #ccc",
                 borderRadius: "4px",
                 boxShadow: "inset 0 1px 1px rgba(0,0,0,.075)",
-                boxSizing: "border-box"
-                }}></input>
-            </div>
-            <div style={{marginBottom: "15px"}}>
-              <label htmlFor="password" style={{display: "block", fontWeight: "700", marginBottom:'2px'}}>Password</label>
-              <input type="password" id="password" onChange={(e) => this.setState({password: e.target.value})} style={{
+                boxSizing: "border-box",
+              }}
+            ></input>
+          </div>
+          <div style={{ marginBottom: "15px" }}>
+            <label
+              htmlFor="password"
+              style={{
+                display: "block",
+                fontWeight: "700",
+                marginBottom: "2px",
+              }}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => this.setState({ password: e.target.value })}
+              style={{
                 display: "block",
                 width: "100%",
                 height: "34px",
@@ -62,29 +93,51 @@ export default class Login extends Component {
                 border: "1px solid #ccc",
                 borderRadius: "4px",
                 boxShadow: "inset 0 1px 1px rgba(0,0,0,.075)",
-                boxSizing: "border-box"
-                }}></input>
-            </div>
-            <div style={{display: "flex", alignContent: "center", textAlign: "auto"}}>
-              <label htmlFor="remember-me">
-              <input id="remember-me" type="checkbox" style={{width: "10px"}}></input>
-                Remember Me</label>
-            </div>
-            <div style={{marginTop: '7px'}}>
-              <button style={{textTransform:' uppercase',
-                backgroundColor: '#fff',
-                padding: '8px 10px',
-                border: 'solid 1px #cdcdcd',
-                color: '#19B6FA',
-                fontSize: '11px',
-                fontWeight: 'bold',
-                letterSpacing: '1.8px',
+                boxSizing: "border-box",
+              }}
+            ></input>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+              textAlign: "auto",
+            }}
+          >
+            <label htmlFor="remember-me">
+              <input
+                id="remember-me"
+                type="checkbox"
+                style={{ width: "10px" }}
+              ></input>
+              Remember Me
+            </label>
+          </div>
+          <div style={{ marginTop: "7px" }}>
+            <button
+              style={{
+                textTransform: " uppercase",
+                backgroundColor: "#fff",
+                padding: "8px 10px",
+                border: "solid 1px #cdcdcd",
+                color: "#19B6FA",
+                fontSize: "11px",
+                fontWeight: "bold",
+                letterSpacing: "1.8px",
                 fontFamily: "Lato",
-                marginRight: "4px"
-                }}>Sign in</button>
-              <a href={'/placeholder'} style={{ textDecoration:"none", color: "#666"}}>Forgot Password?</a>
-            </div>
-          </form>
+                marginRight: "4px",
+              }}
+            >
+              Sign in
+            </button>
+            <a
+              href={"/placeholder"}
+              style={{ textDecoration: "none", color: "#666" }}
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
       </div>
     );
   }
