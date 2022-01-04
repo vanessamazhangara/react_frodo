022d961a0b9d2f3dc29a8e81fe839f86d38af32f
Vanessa Mazhangara <nvmazhan@gmail.com>
2022-01-04 15:45:33 -0500

    vanessa wk 1 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
+import Login from './components/login';
 
 function App() {
   return (
-    <div className="App" >
-      <Login/>
+    <div className="App">
+      <Login />
     </div>
   );
 }
 
 export default App;
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,144 +1,71 @@
 import React, { Component } from "react";
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
+class Login extends Component {
+  constructor() {
+    super();
+
     this.state = {
-      userName: "",
-      password: "",
+      account: { login: "", password: "" },
     };
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("submitted");
+  };
+
+  handleChange = ({currentTarget: input}) => {
+    const account = {...this.state.account};
+    account[input.name]= input.value;
+    this.setState({ account })
+
+  }
+
   render() {
+      const { account } = this.state
     return (
-      <div
-        style={{
-          width: "35%",
-          marginTop: "180px",
-          marginLeft: "6%",
-          textAlign: "left",
-          boxSizing: "border-box",
-        }}
-      >
-        <h1
-          style={{
-            fontWeight: "500",
-            lineHeight: "1.1",
-            fontFamily: "'Montserrat', sans-serif",
-            margin: "0.5em 0",
-            fontSize: "20px",
-          }}
-        >
-          Welcome
-        </h1>
-        <p>Your Devmountain adventure awaits.</p>
-        <form>
-          <div style={{ marginBottom: "15px" }}>
-            <label
-              htmlFor="login"
-              style={{
-                display: "block",
-                fontWeight: "700",
-                marginBottom: "2px",
-              }}
-            >
-              Login
-            </label>
-            <input
-              type="text"
-              id="login"
-              onChange={(e) => this.setState({ userName: e.target.value })}
-              style={{
-                display: "block",
-                width: "100%",
-                height: "34px",
-                padding: "6px 12px",
-                fontSize: "14px",
-                lineHeight: "1.42857143",
-                color: "#555",
-                backgroundColor: "#fff",
-                backgroundImage: "none",
-                border: "1px solid #ccc",
-                borderRadius: "4px",
-                boxShadow: "inset 0 1px 1px rgba(0,0,0,.075)",
-                boxSizing: "border-box",
-              }}
-            ></input>
-          </div>
-          <div style={{ marginBottom: "15px" }}>
-            <label
-              htmlFor="password"
-              style={{
-                display: "block",
-                fontWeight: "700",
-                marginBottom: "2px",
-              }}
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => this.setState({ password: e.target.value })}
-              style={{
-                display: "block",
-                width: "100%",
-                height: "34px",
-                padding: "6px 12px",
-                fontSize: "14px",
-                lineHeight: "1.42857143",
-                color: "#555",
-                backgroundColor: "#fff",
-                backgroundImage: "none",
-                border: "1px solid #ccc",
-                borderRadius: "4px",
-                boxShadow: "inset 0 1px 1px rgba(0,0,0,.075)",
-                boxSizing: "border-box",
-              }}
-            ></input>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignContent: "center",
-              textAlign: "auto",
-            }}
-          >
-            <label htmlFor="remember-me">
+      <div>
+        This Is under Athis.props
+        <div>Column 1</div>
+        <div>
+          Column 2<h1> Welcome</h1>
+          <p> Your journey begins here</p>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="login">Login</label>
               <input
-                id="remember-me"
-                type="checkbox"
-                style={{ width: "10px" }}
+                value={account.login}
+                onChange={this.handleChange}
+                name="login"
+                type="text"
+                id="login"
               ></input>
-              Remember Me
-            </label>
-          </div>
-          <div style={{ marginTop: "7px" }}>
-            <button
-              style={{
-                textTransform: " uppercase",
-                backgroundColor: "#fff",
-                padding: "8px 10px",
-                border: "solid 1px #cdcdcd",
-                color: "#19B6FA",
-                fontSize: "11px",
-                fontWeight: "bold",
-                letterSpacing: "1.8px",
-                fontFamily: "Lato",
-                marginRight: "4px",
-              }}
-            >
-              Sign in
-            </button>
-            <a
-              href={"/placeholder"}
-              style={{ textDecoration: "none", color: "#666" }}
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                value={account.password}
+                onChange={this.handleChange}
+                name="password"
+                type="password"
+                id="password"
+              ></input>
+            </div>
+            <div>
+              <input type="checkbox"></input>
+              <label>Remember Me</label>
+            </div>
+            <div>
+              <button>Sign In</button>
+              <button>Forgot Password</button>
+            </div>
+          </form>
+        </div>
+        <div>Column 3</div>
       </div>
     );
   }
 }
+
+export default Login;
